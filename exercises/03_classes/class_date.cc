f6279612ea1601f27c5bbeb6dfd57f7e9bc70875
Ginevra <ginevracoal@gmail.com>
2017-10-25 22:07:55 +0200

    corrections made

--- exercises/03_classes/class_date.cc
+++ exercises/03_classes/class_date.cc
@@ -16,9 +16,8 @@ Implement a class Date. This class must meet the following requirements:
 using namespace std;
 
 //scoped enum
-enum M{jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
+enum class M{jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
 	
-
 class Date {
 	//these members are private by default
   int day, year;
@@ -28,12 +27,10 @@ class Date {
 	bool is_leap(int year) const{
 		return ((year % 4 ==0) && (year %100 != 0 || year % 400 == 0));
 	}
-
-	Date following_day(); //adds a single day to date
-	int month_days(M month);
-	void print_date(Date date);
-
-
+	
+	//adds one day
+	Date following_day(); 
+	
   public:
 		//constructor declaration
 		Date(int day, M month, int year);
@@ -43,8 +40,20 @@ class Date {
     M get_month() const {return month;}
     int get_year() const {return year;}
 
-    Date add_days(const unsigned int n, Date date);
-		
+		void set_day(int d){d = day;}
+		void set_month(M m){m = month;}
+		void set_year(int y){y = year;}
+
+		Date add_days(const unsigned int n, Date date){
+		int i;
+		for (i=0; i<n; i++)
+			following_day(date);
+		return date;
+		}
+
+		int month_days(M month);
+		void print_date(Date date);
+
 		//destructor
 		~ Date(){}
 };
@@ -75,12 +84,12 @@ int Date::month_days(M month){
 //parametrized constructor definition
 Date::Date(int d, M m, int y): day{d}, month{m}, year{y} {}
 
-void print_date(Date date){
+void Date::print_date(Date date){
 		cout << day << " " << month << " " << year << endl;
 }
 
 //inserire il dettaglio dell'anno bisestile!!
-Date following_day(Date date){
+Date Date::following_day(Date date){
 	if(day > month_days(get_month(date))){
 		get_day(date)=1;
 		++get_month(date);
@@ -97,25 +106,18 @@ Date following_day(Date date){
 }
 
 
-Date Date::add_days(const unsigned int n, Date date){
-	int i;
-	for (i=0; i<n; i++)
-		following_day(date);
-	return date;
-}
-
 
 int main(int argc, char *argv[]){
 	cout << "Insert the date and the number of days.\n";
 	
 	Date date;
 
-	date.setday(atoi(argv[0]));
-	date.setmonth(argv[1]);
-  date.setyear(atoi(argv[2]));
+	date.set_day(atoi(argv[0]));
+	date.set_month(argv[1]);
+  date.set_year(atoi(argv[2]));
 
-  Date.add_days(argv[3], date); //call member function
-	Date.print_date(date);
+  date.add_days(argv[3], date); //call member function
+	date.print_date(date);
 	return 0;
 }
 
