bbc214f5702132390666efc904ad4080bdc75909
Ginevra <ginevracoal@gmail.com>
2017-11-30 15:01:34 +0100

    linked list done

--- exercises/04_error_handling/linked_list.cc
+++ exercises/04_error_handling/linked_list.cc
@@ -2,10 +2,13 @@
 // Linked list
 // Implement a linked list using unique pointers
 
-#include <iostream>
-#include <memory>
+// reset() calls the destructor for the object currently managed and takes
+// the ownership.
+// release() releases ownership of its stored pointer, by returning its
+// value and replacing it with a null pointer.
 
-// using namespace std;
+#include <iostream>
+#include <memory>  // for smart pointers
 
 // scoped enum
 enum class Insertion_method { push_back, push_front };
@@ -16,20 +19,17 @@ class List {
   // implement suitable constructor(s) for List
   List() : _size{0}, head{nullptr} {}
 
-  // insert a new node with the value v according to the method m
-  // this method should be used to fill the list
   void insert(const value_type& v, const Insertion_method m);
+  // passing v by reference because I don't know its size,
+  // otherwise it's better to pass by value.
 
-  // print the values of the nodes
   void print();
 
   // return the size of the list
   unsigned int size() { return _size; }
 
-  // delete all the nodes of the list
   void reset();
 
-  // prune node storing the value v
   void prune_node(const value_type v);
 
  private:
@@ -51,6 +51,8 @@ class List {
   unsigned int _size;          // number of nodes
 };
 
+// insert a new node with the value v according to the method m
+// this method should be used to fill the list
 template <typename value_type>
 void List<value_type>::insert(const value_type& v, const Insertion_method m) {
   switch (m) {
@@ -63,6 +65,7 @@ void List<value_type>::insert(const value_type& v, const Insertion_method m) {
   }
 }
 
+// print the values of the nodes
 template <typename value_type>
 void List<value_type>::print() {
   node* temp = head.get();  // get() returns a pointer to the managed object
@@ -75,28 +78,48 @@ void List<value_type>::print() {
   std::cout << std::endl;
 }
 
+// delete all the nodes of the list
+
+// template <typename value_type>
+// void List<value_type>::reset() {
+//   std::unique_ptr<node> temp = std::move(head);
+//   while (temp) {  // temp != nullptr
+//     temp->next = std::move(temp);
+//   }
+//   temp = nullptr;  // non serve
+//   _size = 0;
+// }
+
 template <typename value_type>
 void List<value_type>::reset() {
-  std::unique_ptr<node> temp = std::move(head);
-  while (temp) {  // temp != nullptr
-    temp->next = std::move(temp);
-  }
-  temp = nullptr;
+  head.reset();
   _size = 0;
 }
 
-// prune_node non funziona
-
+// prune node storing the value v
 template <typename value_type>
 void List<value_type>::prune_node(const value_type v) {
-  node* temp = head.get();
-  while (temp->next) {          // temp->next != nulltemp
-    temp = (temp->next).get();  // move along all the nodes
-    if (temp->val == v) {
-      temp = (temp->next).get();
-      std::unique_ptr<node> ptr = std::move(temp->next);
-      ptr->next = std::move(ptr);
-      _size = _size - 1;
+  if (_size == 1) {
+    head = std::move(head->next);
+    _size = _size - 1;
+  } else {
+    node* previous = head.get();
+    node* temp = head->next.get();
+
+    while (temp->next) {  // temp->next != nullptr
+      if (head->val == v) {
+        previous = previous->next.get();
+        temp = temp->next.get();
+        head = std::move(head->next);
+        _size = _size - 1;
+      }
+      if (temp->val == v) {
+        previous->next.reset(temp->next.release());
+        temp = (previous->next).get();  // move to the next node
+        _size = _size - 1;
+      }
+      temp = temp->next.get();
+      previous = previous->next.get();
     }
   }
 }
